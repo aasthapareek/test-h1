e47ab87b174c237948166ac739a15f803b29a5c8
Aastha Pareek <133286045+aasthapareek@users.noreply.github.com>
2025-09-02 03:42:55 +0530

    Update meow.js

--- api/meow.js
+++ api/meow.js
@@ -3,31 +3,25 @@ export default async function handler(req, res) {
   const results = {};
   
   try {
-    // Check the rapid-client.node file (binary runtime component)
-    try {
-      const rapidStats = fs.statSync('/var/runtime/rapid-client.node');
-      results.rapid_client = {
-        size: rapidStats.size,
-        isFile: rapidStats.isFile(),
-        mode: rapidStats.mode
-      };
-    } catch (error) {
-      results.rapid_client_error = error.message;
+    // Test write access to different directories
+    const testDirs = ['/tmp', '/var/task', '/opt'];
+    
+    for (const dir of testDirs) {
+      try {
+        const testFile = `${dir}/test-write-${Date.now()}.txt`;
+        fs.writeFileSync(testFile, 'test content');
+        fs.unlinkSync(testFile); // Clean up
+        results[`write_${dir.replace('/', '')}`] = 'success';
+      } catch (error) {
+        results[`write_${dir.replace('/', '')}_error`] = error.message;
+      }
     }
     
-    // Look for any AWS or Lambda config files
-    const configPaths = [
-      '/var/runtime/runtime-release',
-      '/proc/self/cgroup',
-      '/proc/self/mountinfo'
-    ];
-    
-    for (const path of configPaths) {
-      try {
-        results[path.replace(/\//g, '_')] = fs.readFileSync(path, 'utf8');
-      } catch (error) {
-        results[path.replace(/\//g, '_') + '_error'] = error.message;
-      }
+    // Check what's actually in the squashfs mounts
+    try {
+      results.var_task_contents = fs.readdirSync('/var/task');
+    } catch (error) {
+      results.var_task_error = error.message;
     }
     
   } catch (error) {
