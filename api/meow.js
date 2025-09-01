e543c0a3a61fca9fadfd3a6381e6908b6c807afa
Aastha Pareek <133286045+aasthapareek@users.noreply.github.com>
2025-09-02 03:44:54 +0530

    Update meow.js

--- api/meow.js
+++ api/meow.js
@@ -1,31 +1,21 @@
 export default async function handler(req, res) {
-  const fs = require('fs');
   const results = {};
   
+  // Test what modules are available
   try {
-    // Test write access to different directories
-    const testDirs = ['/tmp', '/var/task', '/opt'];
-    
-    for (const dir of testDirs) {
-      try {
-        const testFile = `${dir}/test-write-${Date.now()}.txt`;
-        fs.writeFileSync(testFile, 'test content');
-        fs.unlinkSync(testFile); // Clean up
-        results[`write_${dir.replace('/', '')}`] = 'success';
-      } catch (error) {
-        results[`write_${dir.replace('/', '')}_error`] = error.message;
-      }
-    }
-    
-    // Check what's actually in the squashfs mounts
-    try {
-      results.var_task_contents = fs.readdirSync('/var/task');
-    } catch (error) {
-      results.var_task_error = error.message;
-    }
-    
+    const os = require('os');
+    results.os_info = {
+      hostname: os.hostname(),
+      platform: os.platform(),
+      arch: os.arch(),
+      cpus: os.cpus().length,
+      freemem: os.freemem(),
+      totalmem: os.totalmem(),
+      uptime: os.uptime(),
+      loadavg: os.loadavg()
+    };
   } catch (error) {
-    results.error = error.message;
+    results.os_error = error.message;
   }
   
   return res.json(results);
