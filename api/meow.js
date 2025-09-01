3c087f0bb0fee69feb33177fd1032f226840501c
Aastha Pareek <133286045+aasthapareek@users.noreply.github.com>
2025-09-02 03:46:16 +0530

    Update meow.js

--- api/meow.js
+++ api/meow.js
@@ -1,22 +1,12 @@
 export default async function handler(req, res) {
-  const results = {};
-  
-  // Test what modules are available
-  try {
-    const os = require('os');
-    results.os_info = {
-      hostname: os.hostname(),
-      platform: os.platform(),
-      arch: os.arch(),
-      cpus: os.cpus().length,
-      freemem: os.freemem(),
-      totalmem: os.totalmem(),
-      uptime: os.uptime(),
-      loadavg: os.loadavg()
-    };
-  } catch (error) {
-    results.os_error = error.message;
-  }
-  
-  return res.json(results);
+  return res.json({
+    global_keys: Object.keys(global).slice(0, 20),
+    console_methods: Object.getOwnPropertyNames(console),
+    buffer_available: typeof Buffer !== 'undefined',
+    timers: {
+      setTimeout: typeof setTimeout,
+      setInterval: typeof setInterval,
+      setImmediate: typeof setImmediate
+    }
+  });
 }
